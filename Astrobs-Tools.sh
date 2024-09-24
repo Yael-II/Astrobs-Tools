f65039fe95405563b2b62fcc0ab67d518be7ede3
Yaël M. <154762804+Yael-II@users.noreply.github.com>
2024-09-24 16:46:46 +0200

    Add files via upload

--- Astrobs-Tools.sh
+++ Astrobs-Tools.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
 clear
-python source/SCOPE_v2-1.py
+output=$(pip freeze | grep 'astropy==')
+version=${output:9:1}
+if [ $version -ge 6 ]; then
+     python source/SCOPE_v2-1.py
+else
+     printf "\033[91mError: Please use astropy 6.0 or newer version\033[0m\n"
+fi 
